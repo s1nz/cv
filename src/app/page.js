b1e6a8176f6a09c71ac0837d4a7392ac6f58701c
Andrii Poplavskyi <andrii@drink.ch>
2024-07-05 22:49:51 +0200

    first draft

--- src/app/page.js
+++ src/app/page.js
@@ -2,112 +2,90 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="container mx-auto px-6 py-4 lg:px-16 lg:py-12 rounded-lg max-w-[1200px]">
+      <div className="flex mb-16">
+        <div className="w-2/3">
+          <h1 className="text-xl font-bold mb-1">
+           Andrii Poplavskyi
+          </h1>
+          <p className="text-base text-gray-500 mb-1">
+            Front-end Developer
+          </p>
+          <p className="mb-8">
+            Focused on UI/UX, performance, and accessibility.
+          </p>
+          <p className="font-mono text-sm">
+            <a href="mailto:a.poplavskij@gmail.com">a.poplavskij@gmail.com</a>
+            <span className="mx-2 text-gray-500">&bull;</span>
+            <a href="tel:+410774170111">+410774170111</a>
+            <span className="mx-2 text-gray-500">&bull;</span>
+            Uster, Switzerland
+          </p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="lg:flex">
+        <section className="lg:pr-20 lg:w-2/3">
+          <section className="mb-8 text-sm">
+            <p className="font-bold text-base">Skills</p>
+            <p className="text-balance">JavaScript, frameworks (Vue, React, Nuxt.js), Ecommerce, PHP, Magento, Symfony, Headless, HTML, CSS, Ul libraries (Ant Design), preprocessors (SASS, LESS), Webpack, Git, REST, web standards, accessibility standards, responsive and mobile design</p>
+          </section>
+          <article className="mb-8">
+            <h2 className=""><strong className="font-semibold">DRINKS | Silverbogen AG</strong>, Zürich — <strong className="semibold">Lead Front-end developer</strong></h2>
+            <p className="text-gray-500 mb-4 text-sm">08/2019 – present</p>
+            <p className="text-gray-500 mb-4 text-sm"></p>
+            <ul className="list-disc pl-4 text-sm">
+              <li className="mb-2">Design and launch the new website leading to higher customer satisfaction and driving up the sales.</li>
+              <li className="mb-2">In collaboration with team developed and integrated custom headless frontend to simplify development and boosting Magento performance, improving SEO and user engagement.</li>
+              <li className="mb-2">Successfully transitioned the website from Magento 1 to Magento 2, improving stability and security of the website.</li>
+              <li className="mb-2">Developed, integrated, supported multiple in-house applications to support business processes, creating more opportunities for business growth.</li>
+              <li className="mb-2">Leading a small team of developers.</li>
+              <li className="mb-2">Developed and maintained the Market Intelligence Hub, a comprehensive tool providing 360° transparency on products, customers, and pricing for brands.</li>
+              <li className="mb-2">Led the design and implementation of an intuitive dashboard to give users easy access to sales, pricing, and marketing data.</li>
+              <li className="mb-2">Engineered features for real-time customer insights, enabling clients to analyze buyer behavior and sales drivers.</li>
+              <li className="mb-2">Implemented product benchmarking tools to compare performance with market leaders, identifying strengths and areas for improvement.</li>
+              <li className="mb-2">Created dynamic pricing analysis modules to monitor and compare product prices across different European markets.</li>
+              <li className="mb-2">Collaborated with cross-functional teams to ensure seamless integration and optimal performance of all internal tools and features.</li>
+            </ul>
+          </article>
+          <article>
+            <h2 className=""><strong className="font-semibold">Liip AG</strong>, Zürich — <strong className="semibold">Software Developer; Front-end Developer</strong></h2>
+            <p className="text-gray-500 mb-4 text-sm">08/2015 – 08/2019</p>
+            <ul className="list-disc pl-4 text-sm">
+              <li className="mb-2">Development and maintenance of front-end web applications, ecommerce websites, hybrid apps, and a product listing website for the largest retail company in Switzerland</li>
+              <li className="mb-2">Streamlined the process of updating information in the app from weeks to hours, significantly improving efficiency and user experience, reducing costs and time to deploying new content.</li>
+              <li className="mb-2">Collaborated closely with UX teams to design and implement user-friendly and accessible websites</li>
+              <li className="mb-2">Played a key role in simplifying the overall process and enhancing the brand's digital presence.</li>
+            </ul>
+          </article>
+          <article>
+            <h2 className=""><strong className="font-semibold">Eltrino LLC</strong>, Kyiv — <strong className="semibold">Front-end Developer</strong></h2>
+            <p className="text-gray-500 mb-4 text-sm">11/2013 – 08/2015</p>
+          </article>
+          <article>
+            <h2 className=""><strong className="font-semibold">eBay (X.Commerce)</strong>, Kyiv — <strong className="semibold">Front-end Developer</strong></h2>
+            <p className="text-gray-500 mb-4 text-sm">08/2011 – 11/2013</p>
+          </article>
+          <article>
+            <h2 className=""><strong className="font-semibold">Magento</strong>, Kyiv — <strong className="semibold">Front-end Developer</strong></h2>
+            <p className="text-gray-500 mb-4 text-sm">02/2010 – 08/2011</p>
+          </article>
+        </section>
+        <aside className="lg:w-1/3 text-sm">
+          <section className="mb-8 text-sm">
+            <p className="font-bold text-base">Education</p>
+            <p className="font-medium">State University of Information and Communication Technologies</p>
+            <p className="text-gray-500">01/2003 – 12/2008</p>
+            <p>Telecommunications | CS | Masters</p>
+          </section>
+          <section className="mb-8 text-sm">
+            <p className="font-bold text-base">Languages</p>
+            <p>English — C1</p>
+            <p>German — B1</p>
+            <p>Ukrainian — Native</p>
+            <p>Russian — Native</p>
+          </section>
+        </aside>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
